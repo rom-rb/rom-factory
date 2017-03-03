2269b681a3aaccb078cf71041eca7ff5e23a1baa
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-03-03 08:02:45 +0100

    Add byebug

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in rom_factory.gemspec
 gemspec
+
+gem 'byebug', platform: :mri
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,12 @@ require 'pry'
 require 'rom/factory'
 require 'rspec'
 
+begin
+  require 'byebug'
+rescue LoadError
+  # not mri
+end
+
 RSpec.configure do |config|
   config.disable_monkey_patching!
 end
