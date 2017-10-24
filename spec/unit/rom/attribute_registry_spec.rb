5ef00e35bf9238482df0903707451584d1495efb
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-10-24 09:23:04 +0200

    Make sure sequences work with tsorting too

--- spec/unit/rom/attribute_registry_spec.rb
+++ spec/unit/rom/attribute_registry_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ROM::Factory::AttributeRegistry do
   subject(:registry) { ROM::Factory::AttributeRegistry.new(elements) }
 
   let(:elements) do
-    [email_attr, name_attr]
+    [email_attr, id_attr, name_attr]
   end
 
   let(:name_attr) do
@@ -15,9 +15,13 @@ RSpec.describe ROM::Factory::AttributeRegistry do
     attribute(:Callable, :email) { |name| "#{name}@rom-rb.org" }
   end
 
+  let(:id_attr) do
+    attribute(:Sequence, :id) { |n| n }
+  end
+
   describe '#tsort' do
     it 'sorts attributes by their dependencies' do
-      expect(registry.tsort).to eql([name_attr, email_attr])
+      expect(registry.tsort).to eql([name_attr, email_attr, id_attr])
     end
   end
 end
